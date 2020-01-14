bf6d2a8c6b5c32633f1595276788f99d67edd3b4
halvom <halvom@stud.ntnu.no>
2020-01-14 20:11:20 +0100

    Rewrote program completely to avoid memory issues

--- preassignment/pre.jl
+++ preassignment/pre.jl
@@ -7,11 +7,41 @@ import Polynomials
     returing to 0 as a function of time.
 =#
 
+#=
+    Simulates 'N' random walks with maximal length steps and records when each
+    returns to 0. Returns the distribution of how long it took to return.
+=#
+function memory(steps::Int, N::Int)
+    prob = zeros(Int, steps)
+    for i = 1:N
+        sig = 0
+        k = 1
+        num = 0
+        while sig == 0
+            num += 2*rand() - 1
+            sig = sign(num)
+            k += 1
+        end
+        k = 1
+        while k <= steps
+            num += 2*rand() - 1
+            # println(i, ", ", k, ": ", num, ", ", sign(num))
+            # println(k, ", ", num)
+            if sign(num) != sig
+                prob[k] += 1
+                break
+            end
+            k += 1
+        end
+    end
+    return prob
+end
+
 
 # Plots the probability and finds the fitting curve. Assuming p(t) = t^(-k)
 function dataout(steps::Int, prob::Array{Int, 1})
     ln_prob = map(log, prob)
-    t = collect(1:steps-1)
+    t = collect(1:steps)
     ln_t = map(log, t)
 
     # Finds the fitting curve to the data
@@ -26,45 +56,18 @@ function dataout(steps::Int, prob::Array{Int, 1})
 end
 
 
-#=
-    Generates 'N' random walks of length 'steps'. Each step has length between
-    (-1, 1).
-=#
-function randwalk(steps::Int, N::Int)
-    rand_num = map((x)->2*x - 1, rand(steps, N))
-    # rand_num = Random.randn!(Random.MersenneTwister(), zeros(steps, N))
-    return cumsum(rand_num, dims=1)
-end
-
-
-#=
-    Takes a set of random walks and returns the distribution of when each return
-    to zero
-=#
-function prob_return(steps::Int, N::Int, rand_walk::Array{Float64, 2})
-    prob = zeros(Int, steps-1)
-    for j = 1:N
-        sig = 0
-        for i = 1:steps
-            if sig == 0
-                sig = sign(rand_walk[i, j])
-            elseif sign(rand_walk[i, j]) != sig
-                prob[i-1] += 1
-                break
-            end
-        end
-    end
-    return prob
-end
-
 function main()
-    steps = 150                 # Number of steps per walk
-    N = 2000000                  # Number of walks
+    steps = 10000                 # Number of steps per walk
+    N = 40000000                  # Number of walks
 
+    #=
     # Generate random walks
     @time rand_walk = randwalk(steps, N)
     # Find when walk returns to zero
     @time prob = prob_return(steps, N, rand_walk)
+    =#
+
+    @time prob = memory(steps, N)
     # Find fitting curve and present data
     @time dataout(steps, prob)
 end
